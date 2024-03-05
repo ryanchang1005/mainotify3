1da098777c465e95c1b711f6a1a57cd2490de015
ryan <rin841005@gmail.com>
2024-03-05 14:56:14 +0800

    v5

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ryanchang1005/mainotify2
+module github.com/ryanchang1005/mainotify3
 
-go 1.21.6
+go 1.21
--- mainotify.go
+++ mainotify.go
@@ -1,6 +1,6 @@
 package mainotify
 
-import "github.com/ryanchang1005/mainotify2/services/aws"
+import "github.com/ryanchang1005/mainotify3/services/aws"
 
 func NewAWSClient() *aws.AWSClient {
 	return &aws.AWSClient{}
